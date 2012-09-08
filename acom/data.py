b18b3b36485087937641bde8ec7b6719c157f5ea
Michael DeHaan <michael.dehaan@gmail.com>
2012-09-08 12:45:06 -0400

    tweaking data layer

--- acom/data.py
+++ acom/data.py
@@ -23,6 +23,12 @@ class AlreadyExists(DataException):
 class InvalidInput(DataException):
    pass
 
+class DoesNotExist(DataException):
+   pass
+
+class Ambigious(DataException):
+   pass
+
 class Base(object):
 
     def __init__(self):
@@ -31,31 +37,28 @@ class Base(object):
     def cursor(self):
         return conn.cursor()
 
-    def check_required_fields(self, fields):
+    def check_required_fields(self, fields, edit=False):
  
-        if fields.get(self.FIELDS['primary'], None) is None:
-            raise InvalidInput("missing primary field: %s" % self.FIELDS['primary'])
+        if not edit:
+            if fields.get(self.FIELDS['primary'], None) is None:
+                raise InvalidInput("missing primary field: %s" % self.FIELDS['primary'])
      
-        # all required fields are set
-        for f in self.FIELDS['required']:
-            if f not in fields:
-                raise InvalidInput("field %s if required" % f)
+            # all required fields are set
+            for f in self.FIELDS['required']:
+                if f not in fields:
+                    raise InvalidInput("field %s if required" % f)
 
-        # any optional fields get set to defaults if missing
-        for f in self.FIELDS['optional']:
-            if f not in fields:
-                fields[f] = spec['optional'][f] 
+            # any optional fields get set to defaults if missing
+            for f in self.FIELDS['optional']:
+                if f not in fields:
+                    fields[f] = self.FIELDS['optional'][f] 
 
         # no unexpected fields
         for f in fields:
             if f not in self.FIELDS['required'] and f not in self.FIELDS['optional'] and f != self.FIELDS['primary']:
                 raise InvalidInput("invalid field %s" % f)
 
-    def get_by_id(self, id):
-        raise exceptions.NotImplementedError # yet
-
     def add(self, properties):
- 
         self.check_required_fields(properties)
         primary = self.FIELDS['primary']
         matches = self.lookup(properties[primary])
@@ -63,6 +66,54 @@ class Base(object):
             raise AlreadyExists()
         return self._insert(properties)
 
+    def edit(self, name, properties):
+        primary = self.FIELDS['primary']
+        self.check_required_fields(properties, edit=True)
+        if primary in properties:
+            matches = self.lookup(properties[primary])
+            if len(matches):
+                raise AlreadyExists()
+        matches = self.lookup(name)
+        if len(matches) == 0:
+            raise DoesNotExist()
+        elif len(matches) != 1:
+            raise Ambigious()
+        id = matches[0]['id']
+
+        for (k,v) in properties.iteritems():
+            if k not in matches[0]:
+                # TODO: would be nice to execute many here
+                self._insert_kv(id,k,v)
+            elif matches[0][k] != properties[k]:
+                self._update_kv(id,k,v)
+
+    def _insert_kv(self, id, k, v):
+        cur = self.cursor()
+        v=json.dumps(v)
+        sth = """
+           INSERT INTO properties (thing_id, key, value)
+           VALUES (%s,%s,%s)
+        """
+        cur.execute(sth, [id,k,v])
+        conn.commit()
+
+    def _update_kv(self, id, k, v):
+        cur = self.cursor()
+        v=json.dumps(v)
+        sth = """
+            UPDATE properties 
+            SET 
+               value=%s
+            WHERE
+               id IN (
+                    SELECT id FROM properties
+                    WHERE thing_id=%s
+                    AND key=%s
+               )
+        """
+        cur.execute(sth,[v,id,k])
+        conn.commit()
+ 
     def _insert(self, properties):
         primary = self.FIELDS['primary']
         cur = self.cursor()
@@ -136,6 +187,20 @@ class Base(object):
     def lookup(self, value):
         return self.find(self.FIELDS['primary'], value)    
 
+    def delete(self, value):
+        cur = self.cursor()
+        obj = self.find(self.FIELDS['primary'], value)    
+        if len(obj) == 0:
+            pass
+        elif len(obj) != 1:
+            raise Ambigious()
+        id = obj[0]['id']
+        sth = """
+           DELETE FROM thing where id=%s
+        """
+        cur.execute(sth, [id])
+        conn.commit()
+
     def clear(self):
         cur = self.cursor()
         sth = """
@@ -170,7 +235,7 @@ class Junk(Base):
         self.FIELDS = dict(
             primary  = 'name',
             required = [ 'info' ],
-            optional = {},
+            optional = dict(labs=''),
             private = []
         )
 
@@ -178,7 +243,6 @@ class Junk(Base):
 if __name__ == '__main__':
 
     j = Junk()
-
     j.clear()
 
     print 'insert pinky'
@@ -199,10 +263,42 @@ if __name__ == '__main__':
     print 'lookup pinky'
     print j.lookup('pinky')
 
+    print 'edit pinky'
+    j.edit('pinky', dict(info='troz', labs='acme'))
+
+    # again
+    j.edit('pinky', dict(info='fjord', labs='acme'))
+
+    # with invalid fields
+    try:
+        j.edit('pinky', dict(info='fjord', labs='acme', imaginary='...'))
+    except InvalidInput:
+        print 'invalid input blocked'
+        pass
+
+    # with attempting to rename to brain
+    try:
+        j.edit('pinky', dict(name='brain'))
+    except AlreadyExists:
+        print 'rename blocked'
+        pass
+
+    # with attempting to rename to Pinkasso
+    j.edit('pinky', dict(name='pinkasso', info='le narf'))
+
+    print 'lookup pinky'
+    print j.lookup('pinkasso')
+
     print 'lookup brain'
     print j.find('name','brain')
 
     print 'lookup snowball'
     print j.find('name','snowball')
 
+    print 'delete brain'
+    print j.delete('brain')
+
+    print 'list'
+    print j.list()
+ 
 
